c6eec9de4ff22cca60aee1d29283765d047eb955
Jason <jcvalle@mail.usf.edu>
2014-12-21 16:51:52 -0700

    the makings of a game to join

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,13 @@
 (function ($) {
-    var socket = io.connect('http://localhost');
+    var socket = io.connect('http://localhost'),
+        $app = $('.app'),
+        $joinForm = $('.join');
 
     socket.on('newPlaya', function (data) {
         console.log(data);
+    });
+
+    $joinForm.on('submit', function (e) {
+        e.preventDefault();
     });
 })(jQuery);
